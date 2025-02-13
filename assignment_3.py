c1d148f09f46e0e38166444a7e6dbf8f627c5489
prolifyJMA <Jauger2004@gmail.com>
2025-02-13 17:39:01 -0500

    Update assignment_3.py

--- assignment_3.py
+++ assignment_3.py
@@ -1,17 +1,3 @@
-'''
-
-For this assignment please do the following:
-
-- Read in the student_grades.csv file
-
-- calculate the average grade for the class
-
-- for each student record calculate the difference between the student's grade and the average grade
-
-- write the output to a new file called student_grade_differences.csv
-
-'''
-
 import pandas
 
 input_file = "student_grades.csv"
